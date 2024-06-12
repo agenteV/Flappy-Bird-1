b894943edfb5dd19773c593d1b57fe604a662d1e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-12 13:37:37 -0300

    joguinho

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -8,16 +8,34 @@ public class Obstaculo : MonoBehaviour {
     [SerializeField]
     private float variacaoDaPosicaoY;
 
+    
+    private Vector3 posiçãoPassaro;
+
+    private bool pontuei;
+
+    private UASCRIPT scriptDaUI;
     private void Awake()
     {
         this.transform.Translate(Vector3.up * Random.Range(-variacaoDaPosicaoY, variacaoDaPosicaoY));
     }
-
+    private void Start()
+    {
+        this.posiçãoPassaro = GameObject.FindObjectOfType<Bird>().transform.position;
+        this.scriptDaUI = GameObject.FindObjectOfType<UASCRIPT>();
+    }
     private void Update () 
     {
+        if (!this.pontuei && this.transform.position.x < posiçãoPassaro.x)
+        {
+            Debug.Log("Pontuou");
+            this.pontuei = true;
+            this.scriptDaUI.adicionarPontos();
+
+        }
         this.transform.Translate(Vector3.left * this.velocidade * Time.deltaTime);
        
 	}
+
 
     private void OnTriggerEnter2D(Collider2D outro)
     {
